03b8339c8dda04b24394a595757834cb893c2deb
dickolsson <hi@senzilla.io>
2020-02-13 23:02:44 +0100

    Make the tests past

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ Run the tests to see the library at play:
 ```
 $ ./vendor/bin/phpunit
 ```
+
+## Notes
+
+- Code formatting in your IDE might have an impact on the tests (how the fixture is formatted vs. the test output)
--- tests/output/FeatureContactForm.php
+++ tests/output/FeatureContactForm.php
@@ -5,9 +5,9 @@ class FeatureContactForm
 	public function scenarioProvidingValidInput(): array
 	{
 		return [
-		  "Given that I visit the contact page",
-		  "When I fill in the name and message field and submit",
-		  "Then I should get a confirmation message"
+			"Given that I visit the contact page",
+			"When I fill in the name and message field and submit",
+			"Then I should get a confirmation message"
 		];
 	}
 
@@ -15,9 +15,9 @@ class FeatureContactForm
 	public function scenarioProvidingInvalidInput(): array
 	{
 		return [
-		  "Given that I visit the contact page",
-		  "When I fill in the message but no name and submit",
-		  "Then I should get an error message"
+			"Given that I visit the contact page",
+			"When I fill in the message but no name and submit",
+			"Then I should get an error message"
 		];
 	}
 }
